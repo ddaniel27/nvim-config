893d9c46dbeed2156979678b0a78c64f906a102c
ddaniel27 <danieldorado27@gmail.com>
2025-03-29 00:13:02 -0500

    first change to blink.cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,51 +1,45 @@
 return {
-	{
-		'hrsh7th/nvim-cmp',
-		config = function() 
-			require('nvim-autopairs').setup{
-				disable_filetype = { 'TelescopePrompt' , 'guihua', 'guihua_rust', 'clap_input' },
-			}
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
-			-- Setup nvim-cmp.
-			local cmp = require'cmp'
+    version = '1.*',
+    opts = {
+      keymap = { 
+        preset = 'none',
+        ['<CR>'] = { 'select_and_accept', 'fallback' },
+        ['<C-j>'] = { 'select_next' },
+        ['<C-k>'] = { 'select_prev' },
+      },
 
-			cmp.setup{
-				snippet = {
-					expand = function(args)
-						vim.fn['vsnip#anonymous'](args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-					['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i'}),
-					['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i'}),
-				}),
-				sources = {
-					{ name = 'nvim_lsp' },
-					{ name = 'buffer' },
-				},
-			}
+      cmdline = {
+        completion = { menu = { auto_show = true } },
+        keymap = {
+          preset = 'none',
+          ['<CR>'] = { 'accept_and_enter', 'fallback' },
+          ['<C-j>'] = { 'select_next' },
+          ['<C-k>'] = { 'select_prev' },
+        },
+      },
 
-			cmp.setup.cmdline(':', {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = 'path' }
-				}, {
-					{ name = 'cmdline' }
-				})
-			})
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
 
-			local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-		end,
-		dependencies = {
-			'hrsh7th/cmp-cmdline',
-			'windwp/nvim-autopairs',
-			'hrsh7th/cmp-path',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-vsnip',
-			'hrsh7th/vim-vsnip',
-			'hrsh7th/vim-vsnip-integ',
-		},
-	}
+      completion = { documentation = { auto_show = false } },
+
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+      -- signature = { enabled = true },
+    },
+    opts_extend = { "sources.default" },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
 }
