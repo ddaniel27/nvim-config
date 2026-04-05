7ea4c7256ff299c6b27957b99763780ba9985287
ddaniel27 <danieldorado27@gmail.com>
2026-04-04 22:01:40 -0500

    local working plugin

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -1,16 +1,7 @@
 return {
-  "yuukiflow/Arduino-Nvim",
-  dependencies = {
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    -- Load Arduino plugin for .ino files
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "arduino",
-      callback = function()
-        require("Arduino-Nvim")
-        require("Arduino-Nvim.lsp").setup()
-      end,
-    })
-  end,
+  -- "yuukiflow/Arduino-Nvim",
+  dir = vim.fn.expand("$HOME") .. "/personal/Arduino-Nvim",
+  dependencies = { "neovim/nvim-lspconfig" },
+  ft = "arduino",
+  opts = {},
 }
