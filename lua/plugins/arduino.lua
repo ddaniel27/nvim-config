45e4634cd115c50571082c136c75486a8456dc3a
ddaniel27 <danieldorado27@gmail.com>
2026-04-12 11:16:47 -0500

    update config arduino

--- lua/plugins/arduino.lua
+++ lua/plugins/arduino.lua
@@ -1,10 +1,17 @@
-return {
-  -- 'yuukiflow/Arduino-Nvim',
-  dir = vim.fn.expand('$HOME') .. '/personal/Arduino-Nvim',
-  dependencies = { 'neovim/nvim-lspconfig' },
-  ft = 'arduino',
-  lazy = true,
-  opts = {
-    picker_backend = 'fzf'
-  },
-}
+local function should_load()
+  if vim.env.MACHINE_ENV ~= 'personal' then
+    return {}
+  end
+  return {
+    -- 'yuukiflow/Arduino-Nvim',
+    dir = vim.fn.expand('$HOME') .. '/personal/Arduino-Nvim',
+    dependencies = { 'neovim/nvim-lspconfig' },
+    ft = 'arduino',
+    lazy = true,
+    opts = {
+      picker_backend = 'fzf'
+    },
+  }
+end
+
+return should_load()
