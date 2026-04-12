851d7d3b3e9544c64056766021c32b6960a769f3
ddaniel27 <danieldorado27@gmail.com>
2026-04-11 20:14:28 -0500

    remove check for installed parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,26 +3,7 @@ local ts_ensure_installed = { 'go', 'javascript', 'typescript', 'json', 'lua', '
 vim.api.nvim_create_autocmd({ 'Filetype' }, {
   pattern = ts_ensure_installed,
   callback = function(event)
-    local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
-
-    -- no nvim-treesitter, maybe fresh install
-    if not ok then return end
-    
-    local parsers = require('nvim-treesitter.parsers')
-
-    -- not installed and not nvim-treesitter install
-    if not parsers[event.match] or not nvim_treesitter.install then return end
-
-    local ft = vim.bo[event.buf].ft
-    local lang = vim.treesitter.language.get_lang(ft)
-    nvim_treesitter.install({ lang }):await(function(err)
-      if err then
-        vim.notify('Treesitter install error for ft: '..ft..' err: '..err)
-        return
-      end
-
-      pcall(vim.treesitter.start, event.buf)
-    end)
+    vim.treesitter.start()
   end,
 })
 
