63f1ab8a1f1d4cc04f04f5f2b9092139c8cd757e
ddaniel27 <danieldoradolame@gmail.com>
2026-08-07 20:26:01 -0500

    update configs for dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,3 +1,34 @@
+local function get_go_test()
+  local tests_query = [[
+  (function_declaration
+    name: (identifier) @testname
+    parameters: (parameter_list
+      . (parameter_declaration
+        type: (pointer_type) @type) .)
+    (#match? @type "*testing.(T|M)")
+    (#match? @testname "^Test.+$")) @parent
+  ]]
+  
+  local stop_row = vim.api.nvim_win_get_cursor(0)[1]
+  local parser = vim.treesitter.get_parser(0)
+  local root = (parser:parse()[1]):root()
+  local test_query = vim.treesitter.query.parse('go', tests_query)
+
+  local name
+
+  for _, match, _ in test_query:iter_matches(root, 0, 0, stop_row, {all = true}) do
+    for id, nodes in pairs(match) do
+      for _, node in pairs(nodes) do
+        if test_query.captures[id] == "testname" then
+          name = vim.treesitter.get_node_text(node, 0)
+        end
+      end
+    end
+  end
+  
+  return name
+end
+
 local function work_go_dap_config()
 			local dap = require('dap')
       local work_configs = {
@@ -94,6 +125,17 @@ local function personal_go_dap_config()
             { from = '${workspaceFolder}', to = '/app' },
           },
         },
+
+        -- For testing
+        {
+          type = 'gotest',
+          request = 'launch',
+          name = 'Testing func',
+          mode = 'test',
+          program =  './${relativeFileDirname}',
+          outputMode = 'remote',
+          args = function() return { '-test.run=^'.. get_go_test() ..'$'} end,
+        },
       }
 
       for _,config in ipairs(personal_configs) do
@@ -106,6 +148,34 @@ local function personal_go_dap_config()
 				host = '127.0.0.1',
 				port = '2345'
 			}
+
+      dap.adapters.gotest = function(callback, _)
+        local stdout = vim.loop.new_pipe(false)
+        local handle
+        local pid_or_err
+        local port = 38697
+        local opts = {
+          stdio = { nil, stdout },
+          args = { "dap", "-l", "127.0.0.1:" .. port },
+          detached = true,
+        }
+        handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+          stdout:close()
+          handle:close()
+        end)
+        assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+        stdout:read_start(function(err, chunk)
+          assert(not err, err)
+          if chunk then
+            vim.schedule(function()
+              require("dap.repl").append(chunk)
+            end)
+          end
+        end)
+        vim.defer_fn(function()
+          callback { type = "server", host = "127.0.0.1", port = port }
+        end, 100)
+      end
 end
 
 local function load_go_config()
@@ -134,6 +204,28 @@ return {
         bg = 'NONE',
         bold = true,
       })
+      vim.api.nvim_set_hl(0, 'sage_yellow', {
+        fg = '#F6C531',
+        bg = 'NONE',
+        bold = true,
+      })
+      vim.api.nvim_set_hl(0, 'sage_red', {
+        fg = '#AC0715',
+        bg = 'NONE',
+        bold = true,
+      })
+      vim.fn.sign_define(
+        'DapBreakpointRejected',
+        { text = '', texthl = 'sage_red', linehl = '', numhl = '' }
+      )
+      vim.fn.sign_define(
+        'DapBreakpointCondition',
+        { text = '󰯳', texthl = 'sage_green', linehl = '', numhl = '' }
+      )
+      vim.fn.sign_define(
+        'DapLogPoint',
+        { text = '', texthl = 'sage_yellow', linehl = '', numhl = '' }
+      )
       vim.fn.sign_define(
         'DapBreakpoint',
         { text = '', texthl = 'sage_green', linehl = '', numhl = '' }
@@ -159,8 +251,12 @@ return {
             {
               elements = {
                 {
+                  id = 'repl',
+                  size = 0.20,
+                },
+                {
                   id = 'scopes',
-                  size = 0.85,
+                  size = 0.65,
                 },
                 {
                   id = 'breakpoints',
